520846842c563623f82185b5183e445a7537d72a
zennintoji29 <zennintoji29@gmail.com>
2025-09-13 18:22:53 +0530

    Update EmergencyDoctor.tsx

--- ish-swasthya-heal-hub-main/src/pages/EmergencyDoctor.tsx
+++ ish-swasthya-heal-hub-main/src/pages/EmergencyDoctor.tsx
@@ -1,5 +1,6 @@
 import EmergencyChat from "@/components/EmergencyChat";
 
+
 const EmergencyDoctor = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-primary/5 p-6">
